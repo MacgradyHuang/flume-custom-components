b4d1629aa5d711ccdaee69a056c80c2b7c874613
huangpengbo <huangpengbo@gridsum.com>
2019-06-05 21:02:14 +0800

    temp

--- src/main/java/com/simba/flume/source/query/Constants.java
+++ src/main/java/com/simba/flume/source/query/Constants.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 
 public class Constants {
     public static final Integer HANDLER_SERVICE_CORE_POOL_SIZE = 12;
-    public static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+    public static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-ddTHH:mm:ss.SSSZ");
     public static final SimpleDateFormat SDF_YEAR = new SimpleDateFormat("yyyy");
     public static final SimpleDateFormat SDF_MONTH = new SimpleDateFormat("yyyyMM");
     public static final SimpleDateFormat SDF_DAY = new SimpleDateFormat("yyyyMMdd");
@@ -12,5 +12,6 @@ public class Constants {
     public static final Integer TIME_DIFF = 8;
     public static final Integer SCHEDULE_INITIAL_DELAY = 0;
     public static final Integer SCHEDULE_PERIOD = 60;
-    public static final Integer MINUTES_PER_TASK = 60;
+    public static final Integer MINUTES_PER_TASK = 2;
+
 }
